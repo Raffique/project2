87d0cfca0ac5fd889f5c67c283d3a4372608657e
Raffique <raffiquemuir@yahoo.com>
2022-05-02 08:38:08 -0500

    updated files 5

--- src/views/ExploreView.vue
+++ src/views/ExploreView.vue
@@ -2,29 +2,32 @@
     <h2 class="page-header" >Explore</h2>
     <br>
     
-    <div v-if="carsearch != [] " >
-        <div class="container_view">
-        <div v-for="car in carsearch" :key="car.id">
-            <div class = "gridview">
-                <div class="text-left">
-                    <img class="img-top" v-bind:src= "'/uploads/' + car.photo">
-                    <div class="car-body">
-                        <h5 class="card-title">{{ car.year }} {{ car.make }}</h5>
-                        <p>{{ car.model }}</p>
-                        <div class="cost">
-                            {{ car.price }}        
-                        </div>
-                    </div>  
+    
+    <div class="container-fuild">
+        
+            <div v-for="article in articles" :key="article" style="display: inline-block; margin: 10px;" >
+                <div class="card" style="width:300px; height: 480px;">
+                    <img  style="width: 300px; height: 200px;" :src="`/api/uploads/${article.photo.split('/t')[0]}`" alt="Card image">
+                    
+                        <h5 class="card-title">{{ article.title }}</h5>
 
-                    <RouterLink class="Details" v-bind:to="'/cars/' + car.id + '/'+ this.$store.state.uid">
-                        <div class="cardfooter">
+
+                    <div class="card-body">
+                        <h5 class="card-title">{{ article.year }} {{article.make}} <div class="pricetag">${{ article.price }}</div> </h5>
+                        <span class="card-text addresstag"> {{ article.model}}</span>
+                        <br>
+                        
+                    </div>
+                    <div @click="view(article.id)" style="text-decoration-line: none;">
+                        <div class="card-footer footertag">
                             View more details
                         </div>
-                    </RouterLink>
-                </div>
-            </div> 
+                    </div>
+
+                    
+               </div>
             </div>
-            </div>
+            
     </div>
    
     
@@ -35,48 +38,29 @@
     data() {
         return {
           csrf_token: '',
-          link: '',
-          carsearch:[],
+          articles: [],
         };
     },
     created() {
-        this.getCars();
+        this.cars();
         this.getCsrfToken();
         
     },
     methods: {
-        search(){
-            this.carsearch = []
-            let searchForm = document.getElementById('searchForm');
-            let form_data = new FormData(searchForm);
-             fetch('/api/search',{
-                method: 'POST', 
-                body: form_data,
+        
+        cars() {
+            let self = this;
+            fetch('/api/cars', {
+                method: 'GET',
                 headers: {'X-CSRFToken': this.csrf_token}
-             })
-            .then((response) => response.json())
-            .then((data) => {
-               console.log("came to data") 
-               console.log(data);
-               this.carsearch.push(...data)
             })
-            .catch(function (error) {
-                console.log(error);
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                self.articles = data.cars;
             });
         },
-        getCars(){
-            this.cars = []
-      let self = this;
-      fetch('/api/cars')
-        .then((response) => response.json())
-        .then((data) => {
-          console.log("this is the car",data);
-          this.carsearch.push(...data)
-        })
-        .catch(function (error) {
-              console.log(error);
-          });
-    },
     getCsrfToken(){
       //console.log("Logged state",this.$store.state.auth)
       let self = this;
@@ -90,3 +74,95 @@
   }  
     }
 </script>
+
+<style>
+    .btn-container{
+        display: grid;
+        grid-template-columns: 350px 350px 350px;
+        padding: 350px;
+      
+    }
+    
+    .route-btn{
+        border: 2px solid rgba(255, 255, 255, 0.445);
+        background-color:rgba(255, 255, 255, 0.445);
+        padding: 14px 28px;
+        font-size: 20px;
+        cursor: pointer;
+        text-align: center;
+        margin-left: auto;
+        margin-right: auto;
+        font-family:Tahoma;
+        font-weight: bold;
+       
+        
+    }
+
+    .route-btn:hover{
+         top: -10px;
+         position: relative;
+         font-size: 25px;
+    }
+    body{
+        height: 100%;
+    }
+
+    #logout{
+        text-align:right;
+        font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+        right: 30px;
+        position: relative;
+    }
+
+    .nav-link{
+         color: rgba(15, 13, 13, 0.904);
+    }
+
+    .addresstag{
+    margin: 0;
+    padding: 0;
+    color: grey;
+}
+
+
+
+.pricetag{
+    display: inline-block;
+    width: fit-content;
+    background-color:rgb(0, 153, 0); 
+    color: white; 
+    padding: 8px; 
+    border-radius: 15px; 
+    font-size: 12px; 
+    margin-top: 5px; 
+    margin-bottom: 5px;
+    margin-left:40px;
+}
+
+.typetag{
+    margin-left: 10px;
+    width: fit-content;
+    background-color:rgb(245, 159, 0); 
+    color: white; 
+    padding: 8px; 
+    border-radius: 15px; 
+    font-size: 12px; 
+    margin-top: 5px; 
+    margin-bottom: 5px
+}
+
+.footertag{
+    background-color: rgb(15, 179, 255);
+    color: white;
+    text-align: center;
+}
+
+.footertag:hover{
+    background-color: rgb(3, 153, 199);
+}
+
+.footertag:active{
+    background-color: rgb(2, 117, 151);
+}
+
+</style>
